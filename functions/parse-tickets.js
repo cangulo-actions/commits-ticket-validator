e7b01d4e663f48e2ca6c083186c1ef0a7b6932ca
Carlos Angulo Mascarell <carlos.angulo.mascarell@outlook.com>
2024-02-01 01:36:52 +0100

    feat: added feature to label PR with tickets. Fixes #2 (#9)

--- functions/parse-tickets.js
+++ functions/parse-tickets.js
@@ -5,11 +5,18 @@ function parseTickets (ticketsConfig, commitMsg) {
     const anyMatch = commitMsg.match(ticketConfig.pattern)
     if (anyMatch) {
       for (const match of commitMsg.matchAll(ticketConfig.pattern)) {
-        const name = match[1] ?? match[0]
-        const link = ticketConfig.url.replace('$TICKET', name)
-        const existingTicket = tickets.find(ticket => ticket.name === name)
+        const ticket = { }
+        const multipleMatches = match.length > 1
+        if (multipleMatches) {
+          ticket.name = match[1] // get the first capture group
+          ticket.fullMatch = match[0]
+        } else {
+          ticket.name = match[0]
+        }
+        ticket.link = ticketConfig.url.replace('$TICKET', ticket.name)
+        const existingTicket = tickets.find(x => x.name === ticket.name)
         if (!existingTicket) {
-          tickets.push({ name, link })
+          tickets.push(ticket)
         }
       }
     }
